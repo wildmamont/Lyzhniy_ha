e7788fd23462901e4f0374cb3e74682b794825d4
Yakov Borisov <yakov_borisov@mail.ru>
2022-12-05 20:01:53 +0300

    Fomino temp-sensors

--- .homeassistant/scripts/fomino.sh
+++ .homeassistant/scripts/fomino.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . /config/scripts/secure/set_fomino_llt.sh
-for i in '28_e0505c050000_temperature' '28_eb675a050000_temperature' 'sonoff_th1_mqtt_temperature' 'sonoff_th1_mqtt_humidity' 'sonoff_th2_mqtt_temperature' 'sonoff_th2_mqtt_humidity' '28_3c01b55640a2_temperature'
+for i in '28_e0505c050000_temperature' '28_eb675a050000_temperature' 'sonoff_th1_mqtt_temperature' 'sonoff_th1_mqtt_humidity' 'sonoff_th2_mqtt_temperature' 'sonoff_th2_mqtt_humidity' '28_3c01b55640a2_temperature' '28_3c01b5562324_temperature' '28_0000055a67eb_temperature'
 do
 curl -s -o /tmp/sensorf.txt --location --request GET "$HASS_HOST:8123/api/states/sensor.$i" \
 --header "Authorization: Bearer $HASS_LLT" \
